bc3369d8471f48007b0d3bb0c510f16e981cfe11
Niyazi Ülke <52619088+niyaziulke@users.noreply.github.com>
2021-03-16 10:54:50 +0300

    Add ImageFromArray class

--- colorthief.py
+++ colorthief.py
@@ -36,7 +36,7 @@ class ColorThief(object):
                      must implement `read()`, `seek()`, and `tell()` methods,
                      and be opened in binary mode.
         """
-        self.image = Image.open(file)
+        self.image = Image.fromarray(file)
 
     def get_color(self, quality=10):
         """Get the dominant color.
@@ -77,6 +77,53 @@ class ColorThief(object):
         cmap = MMCQ.quantize(valid_pixels, color_count)
         return cmap.palette
 
+class ColorThiefArray(object):
+    """Color thief from numpy array."""
+    def __init__(self, array):
+        """Create one color thief for one image.
+
+        :param array: A numpy array of an image.
+        """
+        self.image = Image.fromarray(array)
+
+    def get_color(self, quality=10):
+        """Get the dominant color.
+
+        :param quality: quality settings, 1 is the highest quality, the bigger
+                        the number, the faster a color will be returned but
+                        the greater the likelihood that it will not be the
+                        visually most dominant color
+        :return tuple: (r, g, b)
+        """
+        palette = self.get_palette(5, quality)
+        return palette[0]
+
+    def get_palette(self, color_count=10, quality=10):
+        """Build a color palette.  We are using the median cut algorithm to
+        cluster similar colors.
+
+        :param color_count: the size of the palette, max number of colors
+        :param quality: quality settings, 1 is the highest quality, the bigger
+                        the number, the faster the palette generation, but the
+                        greater the likelihood that colors will be missed.
+        :return list: a list of tuple in the form (r, g, b)
+        """
+        image = self.image.convert('RGBA')
+        width, height = image.size
+        pixels = image.getdata()
+        pixel_count = width * height
+        valid_pixels = []
+        for i in range(0, pixel_count, quality):
+            r, g, b, a = pixels[i]
+            # If pixel is mostly opaque and not white
+            if a >= 125:
+                if not (r > 250 and g > 250 and b > 250):
+                    valid_pixels.append((r, g, b))
+
+        # Send array to quantize function which clusters values
+        # using median cut algorithm
+        cmap = MMCQ.quantize(valid_pixels, color_count)
+        return cmap.palette
 
 class MMCQ(object):
     """Basic Python port of the MMCQ (modified median cut quantization)
